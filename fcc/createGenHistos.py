3db6860fc31cb4a3d4e486f33e8ad0e4f329c2db
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2016-11-17 14:24:35 +0100

    corrected path for input root files

--- fcc/createGenHistos.py
+++ fcc/createGenHistos.py
@@ -5,8 +5,8 @@ import os
 gROOT.SetBatch()
 
 treeName = "events"
-FileS = "../../pp_h_4l/example/heppy.analyzers.examples.hzz4l.HTo4lGenTreeProducer.HTo4lGenTreeProducer_1/tree.root"
-FileB = "../../pp_zgzg_4l/example/heppy.analyzers.examples.hzz4l.HTo4lGenTreeProducer.HTo4lGenTreeProducer_1/tree.root"
+FileS = "pp_h_4l/example/heppy.analyzers.examples.hzz4l.HTo4lGenTreeProducer.HTo4lGenTreeProducer_1/tree.root"
+FileB = "pp_zgzg_4l/example/heppy.analyzers.examples.hzz4l.HTo4lGenTreeProducer.HTo4lGenTreeProducer_1/tree.root"
 
 Vars = {   
     "lep1vsPt_pt":{"name":"lep1vsPt_pt","title":"p_{T}^{(1)} [GeV]","bin":25,"xmin":0,"xmax":100},
